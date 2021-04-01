a88b0b417b3e17dcffb93b9e68a3de19f0ee159c
Nikolay Motaev <nikolai.motaev@wizart.tech>
2021-04-01 11:17:21 +0300

    init

---
+++ General/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types = 1);
+
+define('WIZART_VERSION', '2.2.1');
+
+use Magento\Framework\Component\ComponentRegistrar as Registrar;
+
+Registrar::register(Registrar::MODULE, "Wizart_General", __DIR__);
